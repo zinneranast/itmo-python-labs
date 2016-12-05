81e0c7d601ab28ac18f74109a67eb73b9aa809ba
Anastasia Zinner <Anastasia.Zinner@emc.com>
2016-12-05 21:56:47 +0300

    finished rectangles

--- rectangle/drawer/filler/drawer.py
+++ rectangle/drawer/filler/drawer.py
@@ -1,4 +1,5 @@
-from Tkinter import *
+from tkinter import *
+import uuid
 
 
 class CoordinateException(Exception):
@@ -19,57 +20,78 @@ def draw(area, rects):
     return canv, root
 
 
-def fill1(canv, area, rects, x, y):
-    minx = area["width"]
-    maxx = 0
-    miny = area["height"]
-    maxy = 0
-
-    xy = {'x': [minx, maxx], 'y': [miny, maxy]}
-    for rect in rects:
-        xy['x'] += [rect['x1'], rect['x2']]
-        xy['y'] += [rect['y1'], rect['y2']]
-
-    for k, v in xy.items():
-        if k == 'x':
-            for i in v:
-                if i < minx and i > x:
-                    minx = i
-                if i > maxx and i < x:
-                    maxx = i
-        else:
-            for i in v:
-                if i < miny and i > y:
-                    miny = i
-                if i > maxy and i < y:
-                    maxy = i
-
-    canv.create_rectangle(minx, miny, maxx, maxy, fill="yellow")
-    return canv
-
-
-def fill2(canv, area, rects, x, y):
-    for rect in rects:
-        if x > rect['x1'] and x < rect['x2'] and y > rect['y1'] and y < rect['y2']:
-            return fill1(canv, area, rects, x, y)
-    canv.create_rectangle(0, 0, area["width"], area["height"], fill="yellow")
-    for rect in rects:
-        canv.create_rectangle(rect['x1'], rect['y1'], rect['x2'], rect['y2'], fill="white")
-    rects.reverse()
-    for rect in rects:
-        canv.create_line(rect['x1'], rect['y1'], rect['x1'], rect['y2'])
-        canv.create_line(rect['x2'], rect['y1'], rect['x2'], rect['y2'])
-        canv.create_line(rect['x1'], rect['y2'], rect['x2'], rect['y2'])
-        canv.create_line(rect['x1'], rect['y1'], rect['x2'], rect['y1'])
-    return canv
-
-
 def fill(canv, area, rects, x, y):
-    xy = {'x': [area["width"], 0], 'y': [area["height"], 0]}
+    all_rects = []
+
+    # assign unique id for background
+    back_id = int(uuid.uuid1().int >> 64)
+    unique_areas = {}
+
+    # form two arrays for storing all x coordinates and all y coordinates
+    xx = [0, area["width"]]
+    yy = [0, area["height"]]
+
     for rect in rects:
-        xy['x'] += [rect['x1'], rect['x2']]
-        xy['y'] += [rect['y1'], rect['y2']]
+        xx += [rect['x1'], rect['x2']]
+        yy += [rect['y1'], rect['y2']]
 
-    #canv.create_rectangle(minx, miny, maxx, maxy, fill="yellow")
+    # sort this arrays
+    xx.sort()
+    yy.sort()
+
+    # form array which contains all rectangles formed by splitting the main field
+    for j in range(len(yy) - 1):
+        for i in range(len(xx) - 1):
+            all_rects.append({'x1': xx[i], 'y1': yy[j], 'x2': xx[i + 1], 'y2': yy[j + 1]})
+
+    # check which rectangles are included in main rectangles
+    point_id = 0
+    for small_rect in all_rects:
+        contains_rect = []
+
+        for rect in rects:
+            if small_rect['x1'] >= rect['x1'] and small_rect['x2'] <= rect['x2'] and small_rect['y1'] >= rect['y1'] and small_rect['y2'] <= \
+                    rect['y2']:
+                contains_rect.append(rect)
+
+        # assign new id for area formed by crossed rectangles
+        id = 0
+        if contains_rect:
+            for i in contains_rect:
+                id += i['id']
+        else:
+            id = back_id
+
+        # form new array
+        if id in unique_areas:
+            unique_areas[id] += [small_rect]
+        else:
+            unique_areas[id] = [small_rect]
+
+        if x >= small_rect['x1'] and x <= small_rect['x2'] and y >= small_rect['y1'] and y <= small_rect['y2']:
+            point_id = id
+
+    # check whether two rectangles are adjacent
+    def check(canv, rect1, rect2):
+        if (rect1['x2'] == rect2['x1'] and (rect1['y1'] < rect2['y2'] or rect1['y2'] > rect2['y1'])) or (
+                        rect1['x1'] == rect2['x2'] and (rect1['y1'] < rect2['y2'] or rect1['y2'] > rect2['y1'])) or (
+                        rect1['y1'] == rect2['y2'] and (rect1['x1'] > rect2['x2'] or rect1['x2'] < rect2['x1'])) or (
+                        rect1['y2'] == rect2['y1'] and (rect1['x1'] > rect2['x2'] or rect1['x2'] < rect2['x1'])):
+            canv.create_rectangle(rect1['x1'], rect1['y1'], rect1['x2'], rect1['y2'], fill="black")
+            canv.create_rectangle(rect2['x1'], rect2['y1'], rect2['x2'], rect2['y2'], fill="black")
+            return canv
+
+        if x >= rect1['x1'] and x <= rect1['x2'] and y >= rect1['y1'] and y <= rect1['y2']:
+            canv.create_rectangle(rect1['x1'], rect1['y1'], rect1['x2'], rect1['y2'], fill="black")
+        else:
+            canv.create_rectangle(rect2['x1'], rect2['y1'], rect2['x2'], rect2['y2'], fill="black")
+
+        return canv
+
+    # draw
+    if len(unique_areas[point_id]) == 2:
+        return check(canv, unique_areas[point_id][0], unique_areas[point_id][1])
+    for rect in unique_areas[point_id]:
+        canv.create_rectangle(rect['x1'], rect['y1'], rect['x2'], rect['y2'], fill="black")
+
     return canv
-
--- rectangle/drawer/placer/rectangleplacer.py
+++ rectangle/drawer/placer/rectangleplacer.py
@@ -1,5 +1,6 @@
 from rectangle.drawer.filereader.txtfilereader import read
 from rectangle.drawer.filler.drawer import *
+import uuid
 
 
 def place():
@@ -23,7 +24,7 @@ def place():
             y2 = int(data[i + 3])
             if x1 > area['width'] or x2 > area['width'] or y1 > area['height'] or y2 > area['height']:
                 raise CoordinateException
-            rects.append({'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2})
+            rects.append({'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2, 'id': uuid.uuid1().int >> 64})
             i += 4
 
     canv, root = draw(area, rects)
